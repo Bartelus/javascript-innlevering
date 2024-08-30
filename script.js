dfdd9ff415bdfcb8a4c5f7fe5d8edf1bc460737a
Bartelus <wilroed@gmail.com>
2024-08-30 12:00:15 +0200

    Console.log.demonstrations

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
+console.log('Welcome Noob');
+
+// send og return av primære av variabler til console log samlet under hovedkomponent:
+
 let user = {
   online: true,
   accesslevel: 20,
-  class: 'Noob',
+  grade: 'Noob',
 };
 
-console.log('Hello!');
+console.log(user)
+
+// return av primære variabler til console log separat:
+
+let online = true
+let accesslevel = 100
+let grade = 'pro'
+
+console.log(online)
+console.log(accesslevel)
+console.log(grade)
+
+// bruk av const
+
+const banned = true
+
+console.log(banned, "You are permanently banned from this website")
+
